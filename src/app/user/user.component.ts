d2c6e41e22abc188727d266f58d14d33044b7bcd
Fabio <fabiorcolombini@gmail.com>
2024-07-03 16:27:07 -0300

    First Commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+  users = DUMMY_USERS;
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  constructor() {}
+
+  onSelectUser(user: any) {
+    this.selectedUser = user;
+    console.log('User selected:', user);
+  }
+}
